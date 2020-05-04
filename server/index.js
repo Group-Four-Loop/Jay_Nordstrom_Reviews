74a8e38935d86ed2bca5785d1c21eff0047bfaf7
Jenine Steinberg <jenine.steinberg@gmail.com>
2020-05-03 23:21:38 -0700

    sending server calls

--- server/index.js
+++ server/index.js
@@ -10,5 +10,15 @@ const PORT = 3000;
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//to parse json data
+app.use(express.json());
+
+app.get('/products', (req, res) => {
+  getAll((data) => {
+    res.send(data);
+  })
+})
+
 // Start the server on the provided port
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
